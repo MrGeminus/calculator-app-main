25db4cac48640c08a9d0c2230428c905ad1a24ac
Uros Tomic <urostomic94@gmail.com>
2021-05-25 20:08:21 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ keypadButtons.forEach(keypadButton => {
         let screenText = document.querySelector(".calc-screen-text");
         let key = keypadButton.textContent;
         console.log(screenText.textContent.length)
-        if (isNum.exec(key) && Number(screenText.textContent) === 0) {
+        if (isNum.test(key) && Number(screenText.textContent) === 0) {
             screenText.textContent = key.trim();
             console.log(screenText.textContent.length)
         }
-        else if (isNum.exec(key) && Number(screenText.textContent) !== 0 && screenText.textContent.length <= 2) {
+        else if (isNum.test(key) && Number(screenText.textContent) !== 0 && screenText.textContent.length <= 2) {
             screenText.textContent = screenText.textContent.trim() + key.trim();
             console.log(screenText.textContent.length)
         }
-        else if (isNum.exec(key) && screenText.textContent.length == 3) {
+        else if (isNum.test(key) && screenText.textContent.length == 3) {
             screenText.textContent = screenText.textContent.trim() + comma.trim() + key.trim();
             console.log(screenText.textContent.length)
         }
-        else if (isNum.exec(key) && screenText.textContent.length <= 6 && screenText.textContent.length > 3) {
+        else if (isNum.test(key) && screenText.textContent.length <= 6 && screenText.textContent.length > 3) {
             screenText.textContent = screenText.textContent.trim() + key.trim();
             console.log(screenText.textContent.length)
         }
